b7083508a8916bb5e93839d0becfe0bf704f5357
M-Amer <mohamedamerjalal@gmail.com>
2025-01-16 14:50:51 +0200

    Get Mars photos from the repository
    This commit introduces a MarsPhotosRepository interface and a NetworkMarsPhotosRepository implementation to abstract the data source for Mars photos.
    
    The MarsViewModel now uses the NetworkMarsPhotosRepository to retrieve the Mars photos.

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -5,7 +5,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.marsphotos.network.MarsApi
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
 import kotlinx.coroutines.launch
 
 sealed interface MarsUiState {
@@ -25,7 +25,7 @@ class MarsViewModel : ViewModel() {
     private fun getMarsPhotos() {
         viewModelScope.launch {
             marsUiState = try {
-                val listResult = MarsApi.retrofitService.getPhotos()
+                val listResult = NetworkMarsPhotosRepository().getMarsPhotos()
                 MarsUiState.Success("Success: ${listResult.size} Mars photos retrieved")
             } catch (_: Exception) {
                 MarsUiState.Error
